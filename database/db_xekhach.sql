8707378d6329ff85fff5cd884bf947d82fb953dd
Moobbot <ngotam24082001@gmai.com>
2022-03-05 22:08:52 +0700

    form

--- database/db_xekhach.sql
+++ database/db_xekhach.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 03, 2022 at 03:37 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 05, 2022 lúc 10:31 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,29 +18,39 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_xekhach`
+-- Cơ sở dữ liệu: `db_xekhach`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chuyenxe`
+-- Cấu trúc bảng cho bảng `chuyenxe`
 --
 
 CREATE TABLE `chuyenxe` (
   `ma_chuyenxe` int(11) NOT NULL,
-  `ma_xe` int(11) NOT NULL,
-  `ma_tuyenxe` int(11) NOT NULL,
-  `ghetrong_chuyenxe` int(11) NOT NULL,
+  `ma_tuyenduong` int(11) NOT NULL,
   `giatien_chuyenxe` int(11) NOT NULL,
-  `giodi_chuyenxe` time NOT NULL,
-  `gioden_chuyenxe` time NOT NULL
+  `giodi_chuyenxe` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nhaxe`
+-- Cấu trúc bảng cho bảng `chuyenxechay`
+--
+
+CREATE TABLE `chuyenxechay` (
+  `ma_xe` int(11) NOT NULL,
+  `ma_chuyenxe` int(11) NOT NULL,
+  `ghetrong_chuyenxe` int(11) NOT NULL,
+  `trangthai` enum('0','1','2','3') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhaxe`
 --
 
 CREATE TABLE `nhaxe` (
@@ -54,44 +64,37 @@ CREATE TABLE `nhaxe` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `taikhoan`
+-- Cấu trúc bảng cho bảng `taikhoan`
 --
 
 CREATE TABLE `taikhoan` (
   `ten_taikhoan` varchar(20) NOT NULL,
-  `matkhau_taikhoan` varchar(600) NOT NULL
+  `matkhau` varchar(600) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `taikhoan`
---
-
-INSERT INTO `taikhoan` (`ten_taikhoan`, `matkhau`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tuyenduong`
+-- Cấu trúc bảng cho bảng `tuyenduong`
 --
 
 CREATE TABLE `tuyenduong` (
   `ma_tuyenduong` int(11) NOT NULL,
   `diemdi_tuyenduong` text NOT NULL,
   `diemden_tuyenduong` text NOT NULL,
-  `km_tuyenduong` int(11) NOT NULL
+  `hinhanh_tuyenduong` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vexe`
+-- Cấu trúc bảng cho bảng `vexe`
 --
 
 CREATE TABLE `vexe` (
   `ma_ve` int(11) NOT NULL,
   `ma_chuyenxe` int(11) NOT NULL,
-  `diendon_khach` varchar(50) NOT NULL,
+  `diemdon_khach` varchar(50) NOT NULL,
   `gia_ve` int(11) NOT NULL,
   `hoten_khach` varchar(50) NOT NULL,
   `sdt_khach` varchar(11) NOT NULL,
@@ -102,7 +105,7 @@ CREATE TABLE `vexe` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `xe`
+-- Cấu trúc bảng cho bảng `xe`
 --
 
 CREATE TABLE `xe` (
@@ -113,72 +116,83 @@ CREATE TABLE `xe` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `chuyenxe`
+-- Chỉ mục cho bảng `chuyenxe`
 --
 ALTER TABLE `chuyenxe`
-  ADD PRIMARY KEY (`ma_chuyenxe`);
+  ADD PRIMARY KEY (`ma_chuyenxe`),
+  ADD KEY `foreign key1` (`ma_tuyenduong`);
 
 --
--- Indexes for table `nhaxe`
+-- Chỉ mục cho bảng `chuyenxechay`
+--
+ALTER TABLE `chuyenxechay`
+  ADD KEY `foreign key2` (`ma_chuyenxe`),
+  ADD KEY `foreign key3` (`ma_xe`);
+
+--
+-- Chỉ mục cho bảng `nhaxe`
 --
 ALTER TABLE `nhaxe`
   ADD PRIMARY KEY (`ma_nhaxe`);
 
 --
--- Indexes for table `tuyenduong`
+-- Chỉ mục cho bảng `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD PRIMARY KEY (`ten_taikhoan`);
+
+--
+-- Chỉ mục cho bảng `tuyenduong`
 --
 ALTER TABLE `tuyenduong`
   ADD PRIMARY KEY (`ma_tuyenduong`);
 
 --
--- Indexes for table `vexe`
+-- Chỉ mục cho bảng `vexe`
 --
 ALTER TABLE `vexe`
-  ADD PRIMARY KEY (`ma_ve`);
+  ADD PRIMARY KEY (`ma_ve`),
+  ADD KEY `foreign key4` (`ma_chuyenxe`);
 
 --
--- Indexes for table `xe`
+-- Chỉ mục cho bảng `xe`
 --
 ALTER TABLE `xe`
-  ADD PRIMARY KEY (`ma_xe`);
+  ADD PRIMARY KEY (`ma_xe`),
+  ADD KEY `ma_nhaxe` (`ma_nhaxe`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `chuyenxe`
+-- Các ràng buộc cho bảng `chuyenxe`
 --
 ALTER TABLE `chuyenxe`
-  MODIFY `ma_chuyenxe` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `foreign key1` FOREIGN KEY (`ma_tuyenduong`) REFERENCES `tuyenduong` (`ma_tuyenduong`);
 
 --
--- AUTO_INCREMENT for table `nhaxe`
+-- Các ràng buộc cho bảng `chuyenxechay`
 --
-ALTER TABLE `nhaxe`
-  MODIFY `ma_nhaxe` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `chuyenxechay`
+  ADD CONSTRAINT `foreign key2` FOREIGN KEY (`ma_chuyenxe`) REFERENCES `chuyenxe` (`ma_chuyenxe`),
+  ADD CONSTRAINT `foreign key3` FOREIGN KEY (`ma_xe`) REFERENCES `xe` (`ma_xe`);
 
 --
--- AUTO_INCREMENT for table `tuyenduong`
---
-ALTER TABLE `tuyenduong`
-  MODIFY `ma_tuyenduong` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `vexe`
+-- Các ràng buộc cho bảng `vexe`
 --
 ALTER TABLE `vexe`
-  MODIFY `ma_ve` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `foreign key4` FOREIGN KEY (`ma_chuyenxe`) REFERENCES `chuyenxe` (`ma_chuyenxe`);
 
 --
--- AUTO_INCREMENT for table `xe`
+-- Các ràng buộc cho bảng `xe`
 --
 ALTER TABLE `xe`
-  MODIFY `ma_xe` int(11) NOT NULL AUTO_INCREMENT;
+  ADD CONSTRAINT `xe_ibfk_1` FOREIGN KEY (`ma_nhaxe`) REFERENCES `nhaxe` (`ma_nhaxe`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
