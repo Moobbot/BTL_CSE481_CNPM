eee3af2fe1a86e7f3d03832488233238a3337c94
Moobbot <ngotam24082001@gmai.com>
2022-03-05 12:34:48 +0700

    fig wanning

--- wibu/core/App.php
+++ wibu/core/App.php
@@ -1,32 +1,32 @@
 <!-- 
     //*Code tập trung tại đây
+    //* Nơi xử lý thanh địa chỉ.
     //*Nơi gọi controllers, modals, views
 -->
 <?php
 class App
 {
-    //http://localhost/BTL_CSE481_CNPM/home/SayHi/4
     protected $controller = "home"; //protected để bảo mật
     protected $action = "SayHi";
     protected $params = [];
 
     function __construct()
     {
-        // Array ( [0] => home [1] => SayHi [2] => 4 )
         $arr = $this->UrlProcess();
-        // print_r($arr);
+        //Controller
 
-        //Xử lí Controller
         //Kiểm tra file có tồn tại không.
-        if (file_exists("./wibu/controllers/" . $arr[0] . ".php")) {
+
+        if (isset($arr) && file_exists("./wibu/controllers/" . $arr[0] . ".php")) {
             $this->controller = $arr[0];
-            //Sau khi chạy hủy đi cho chuỗi dưới ko còn nữa
             unset($arr[0]);
         }
+
         require_once "./wibu/controllers/" . $this->controller . ".php";
         $this->controller = new $this->controller;
 
-        //Xử lí Action - được chạy
+        //Action - được chạy
+
         if (isset($arr[1])) {
             //Kiểm tra tồn tại hàm không
             if (method_exists($this->controller, $arr[1])) {
@@ -35,7 +35,7 @@ class App
             unset($arr[1]);
         }
 
-        //Xử lí Params
+        //Params
         $this->params = $arr ? array_values($arr) : [];
 
         call_user_func_array([$this->controller, $this->action], $this->params);
@@ -44,11 +44,9 @@ class App
     // function xử lý url
     function UrlProcess()
     {
-        //home/SayHi/4
         if (isset($_GET["url"])) {
             //Loại bỏ khoảng trắng - trim
-            //Cắt chuỗi theo dấu "/"
-            return explode("/", filter_var(trim($_GET["url"], "/")));
+            return explode("/", filter_var(trim($_GET["url"], "/"))); //Cắt chuỗi theo dấu "/"
         }
     }
 }
