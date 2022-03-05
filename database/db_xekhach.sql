f2d1e89bcbf94ec1a29dd154b7600b1345db5b22
Moobbot <ngotam24082001@gmai.com>
2022-03-05 23:21:32 +0700

    update SQL

--- database/db_xekhach.sql
+++ database/db_xekhach.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2022 at 04:53 PM
+-- Generation Time: Mar 05, 2022 at 05:19 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `chuyenxe` (
-  `ma_chuyenxe` int(11) NOT NULL,
-  `ma_tuyenduong` int(11) NOT NULL,
+  `ma_chuyenxe` int(11) UNSIGNED NOT NULL,
+  `ma_tuyenduong` int(11) UNSIGNED NOT NULL,
   `giatien_chuyenxe` int(11) NOT NULL,
   `giodi_chuyenxe` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -41,9 +41,9 @@ CREATE TABLE `chuyenxe` (
 --
 
 CREATE TABLE `chuyenxechay` (
-  `ma_xe` int(11) NOT NULL,
-  `ma_chuyenxe` int(11) NOT NULL,
-  `soghetrong` int(11) NOT NULL,
+  `ma_xe` int(11) UNSIGNED NOT NULL,
+  `ma_chuyenxe` int(11) UNSIGNED NOT NULL,
+  `soghetrong` int(11) UNSIGNED NOT NULL,
   `trangthai` enum('0','1','2','3') DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -54,7 +54,7 @@ CREATE TABLE `chuyenxechay` (
 --
 
 CREATE TABLE `nhaxe` (
-  `ma_nhaxe` int(11) NOT NULL,
+  `ma_nhaxe` int(11) UNSIGNED NOT NULL,
   `ten_nhaxe` varchar(30) NOT NULL,
   `sdt_nhaxe` varchar(11) NOT NULL,
   `diachi_nhaxe` text NOT NULL,
@@ -86,7 +86,7 @@ INSERT INTO `taikhoan` (`ten_taikhoan`, `matkhau`) VALUES
 --
 
 CREATE TABLE `tuyenduong` (
-  `ma_tuyenduong` int(11) NOT NULL,
+  `ma_tuyenduong` int(11) UNSIGNED NOT NULL,
   `diemdi_tuyenduong` text NOT NULL,
   `diemden_tuyenduong` text NOT NULL,
   `hinhanh_tuyenduong` tinytext NOT NULL
@@ -99,8 +99,8 @@ CREATE TABLE `tuyenduong` (
 --
 
 CREATE TABLE `vexe` (
-  `ma_ve` int(11) NOT NULL,
-  `ma_chuyenxe` int(11) NOT NULL,
+  `ma_ve` int(11) UNSIGNED NOT NULL,
+  `ma_chuyenxe` int(11) UNSIGNED NOT NULL,
   `diemdon_khach` varchar(50) NOT NULL,
   `gia_ve` int(11) NOT NULL,
   `hoten_khach` varchar(50) NOT NULL,
@@ -116,8 +116,8 @@ CREATE TABLE `vexe` (
 --
 
 CREATE TABLE `xe` (
-  `ma_xe` int(11) NOT NULL,
-  `ma_nhaxe` int(11) NOT NULL,
+  `ma_xe` int(11) UNSIGNED NOT NULL,
+  `ma_nhaxe` int(11) UNSIGNED NOT NULL,
   `bienso_xe` varchar(10) NOT NULL,
   `soghe_xe` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -131,26 +131,20 @@ CREATE TABLE `xe` (
 --
 ALTER TABLE `chuyenxe`
   ADD PRIMARY KEY (`ma_chuyenxe`),
-  ADD KEY `foreign key1` (`ma_tuyenduong`);
+  ADD KEY `ma_tuyenduong` (`ma_tuyenduong`);
 
 --
 -- Indexes for table `chuyenxechay`
 --
 ALTER TABLE `chuyenxechay`
-  ADD KEY `foreign key2` (`ma_chuyenxe`),
-  ADD KEY `foreign key3` (`ma_xe`);
+  ADD KEY `ma_chuyenxe` (`ma_chuyenxe`),
+  ADD KEY `ma_xe` (`ma_xe`);
 
 --
 -- Indexes for table `nhaxe`
 --
 ALTER TABLE `nhaxe`
   ADD PRIMARY KEY (`ma_nhaxe`);
-
---
--- Indexes for table `taikhoan`
---
-ALTER TABLE `taikhoan`
-  ADD PRIMARY KEY (`ten_taikhoan`);
 
 --
 -- Indexes for table `tuyenduong`
@@ -163,7 +157,7 @@ ALTER TABLE `tuyenduong`
 --
 ALTER TABLE `vexe`
   ADD PRIMARY KEY (`ma_ve`),
-  ADD KEY `foreign key4` (`ma_chuyenxe`);
+  ADD KEY `ma_chuyenxe` (`ma_chuyenxe`);
 
 --
 -- Indexes for table `xe`
@@ -173,6 +167,40 @@ ALTER TABLE `xe`
   ADD KEY `ma_nhaxe` (`ma_nhaxe`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chuyenxe`
+--
+ALTER TABLE `chuyenxe`
+  MODIFY `ma_chuyenxe` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nhaxe`
+--
+ALTER TABLE `nhaxe`
+  MODIFY `ma_nhaxe` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tuyenduong`
+--
+ALTER TABLE `tuyenduong`
+  MODIFY `ma_tuyenduong` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vexe`
+--
+ALTER TABLE `vexe`
+  MODIFY `ma_ve` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `xe`
+--
+ALTER TABLE `xe`
+  MODIFY `ma_xe` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -180,20 +208,20 @@ ALTER TABLE `xe`
 -- Constraints for table `chuyenxe`
 --
 ALTER TABLE `chuyenxe`
-  ADD CONSTRAINT `foreign key1` FOREIGN KEY (`ma_tuyenduong`) REFERENCES `tuyenduong` (`ma_tuyenduong`);
+  ADD CONSTRAINT `chuyenxe_ibfk_1` FOREIGN KEY (`ma_tuyenduong`) REFERENCES `tuyenduong` (`ma_tuyenduong`);
 
 --
 -- Constraints for table `chuyenxechay`
 --
 ALTER TABLE `chuyenxechay`
-  ADD CONSTRAINT `foreign key2` FOREIGN KEY (`ma_chuyenxe`) REFERENCES `chuyenxe` (`ma_chuyenxe`),
-  ADD CONSTRAINT `foreign key3` FOREIGN KEY (`ma_xe`) REFERENCES `xe` (`ma_xe`);
+  ADD CONSTRAINT `chuyenxechay_ibfk_1` FOREIGN KEY (`ma_chuyenxe`) REFERENCES `chuyenxe` (`ma_chuyenxe`),
+  ADD CONSTRAINT `chuyenxechay_ibfk_2` FOREIGN KEY (`ma_xe`) REFERENCES `xe` (`ma_xe`);
 
 --
 -- Constraints for table `vexe`
 --
 ALTER TABLE `vexe`
-  ADD CONSTRAINT `foreign key4` FOREIGN KEY (`ma_chuyenxe`) REFERENCES `chuyenxe` (`ma_chuyenxe`);
+  ADD CONSTRAINT `vexe_ibfk_1` FOREIGN KEY (`ma_chuyenxe`) REFERENCES `chuyenxe` (`ma_chuyenxe`);
 
 --
 -- Constraints for table `xe`
