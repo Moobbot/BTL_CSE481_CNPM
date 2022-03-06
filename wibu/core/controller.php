32a93c7988c0f5ad6388562b3323cd7c44feb1ed
DuyLoR <duya7k50@gmail.com>
2022-03-06 22:39:16 +0700

    bug

---
+++ wibu/core/controller.php
@@ -0,0 +1,17 @@
+<!-- Hàm gọi modal, view -->
+<?php
+class Controller
+{
+    //Hàm gọi model
+    public function model($model)
+    {
+        require_once "./wibu/models/" . $model . ".php";
+        return new $model; //<=> $a = new $model;
+    }
+
+    //Hàm gọi view
+    public function view($view, $data = [])
+    {
+        require_once "./wibu/views/" . $view . ".php";
+    }
+}
