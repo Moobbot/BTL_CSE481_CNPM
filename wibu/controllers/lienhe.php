b3f7dd3c87d7d630b470fa56ffac08c62ba31374
DuyLoR <duya7k50@gmail.com>
2022-03-06 21:55:51 +0700

    merge main

--- wibu/controllers/lienhe.php
+++ wibu/controllers/lienhe.php
@@ -3,21 +3,11 @@ class lienhe extends Controller
 {
     function SayHi()
     {
-<<<<<<< HEAD
-        $lienhe = $this->view("lienhe");
-
-    }
-    function ResultForm(){
-        if(isset($_POST["submit"])){
-            
-        }
-=======
         $this->view(
             "page_khach",
             [
                 "Page" => "lienheView"
             ]
         );
->>>>>>> main
     }
 }
