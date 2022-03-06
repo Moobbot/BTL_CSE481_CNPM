d199aa61dfeba04d5e46f6ae43063dff9ae28f6e
DuyLoR <duya7k50@gmail.com>
2022-03-06 20:50:49 +0700

    thay doi

--- wibu/core/controller.php
+++ wibu/core/controller.php
@@ -1,17 +1,12 @@
-<!-- Hàm gọi modal, view -->
 <?php
-class Controller
-{
-    //Hàm gọi model
-    public function model($model)
-    {
-        require_once "./wibu/models/" . $model . ".php";
-        return new $model; //<=> $a = new $model;
+class Controller {
+    public function model($model){
+        require_once "./wibu/models/".$model.".php";
+        return new $model;
     }
 
-    //Hàm gọi view
-    public function view($view, $data = [])
-    {
-        require_once "./wibu/views/" . $view . ".php";
+    public function view($view){
+        require_once "./wibu/views/".$view.".php"; 
     }
 }
+?>
