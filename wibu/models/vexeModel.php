5189173944880cef6120f4889f913282fff89f95
Moobbot <ngotam24082001@gmai.com>
2022-03-08 12:34:08 +0700

    datve

---
+++ wibu/models/vexeModel.php
@@ -0,0 +1,19 @@
+<!-- Chứa các hàm liên quan đến bảng vé xe -->
+<?php
+class vexeModel extends Database
+{
+    public function Newdatve($username, $phone, $cccd, $address, $email)
+    {
+        $sql = "INSERT INTO vexe
+                (ma_chuyenxe, diemdon_khach, hoten_khach, sdt_khach, email_khach, cmt_khach) 
+                VALUES (1,'$address','$username','$phone','$email','$cccd')
+                ";
+
+        $result = false;
+        if (mysqli_query($this->con, $sql)) {
+            $result = true;
+        }
+
+        return json_encode($result);
+    }
+}
