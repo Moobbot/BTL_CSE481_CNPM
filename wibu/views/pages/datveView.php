8b9d73a29f37f1227408a5a8793e6931ae56be25
Moobbot <ngotam24082001@gmai.com>
2022-03-08 12:30:03 +0700

    datve

--- wibu/views/pages/datveView.php
+++ wibu/views/pages/datveView.php
@@ -1,5 +1,5 @@
 <div class="container w-75">
-    <form class="">
+    <div class="">
         <fieldset class="border border-dark rounded-3 p-3">
             <legend class="float-none w-auto px-3 fw-bold">Thông tin chuyến xe</legend>
             <div class="container-fluid">
@@ -10,110 +10,87 @@
                     <div class="col-md-6">
                         <label for="" class="fs-3" id="Name_xekhach" style="font-weight: 600;">Xe khách Hoàng Hà</label>
                         <br>
+
                         <label for="" class="fs-5" id="Name_xekhach" style="font-weight: 600;">Xe khách 30 chỗ </label>
                         <br>
                         <label for="" class="fs-5" id="Name_xekhach" style="font-weight: 600;">Giờ khởi hành : 15h30</label>
                         <br>
-                        <div class="mt-2">
-                            <label for="" class="fs-5 px-3 pb-1 rounded border border-warning bg-warning m-3" id="Name_xekhach" style="font-weight: 600;">Sạch sẽ</label>
-                            <label for="" class="fs-5 px-3 pb-1 rounded border border-warning bg-warning m-3" id="Name_xekhach" style="font-weight: 600;">An toàn</label>
-                            <label for="" class="fs-5 px-3 pb-1 rounded border border-warning bg-warning m-3" id="Name_xekhach" style="font-weight: 600;">Chất lượng</label>
-                            <label for="" class="fs-5 px-3 pb-1 rounded border border-warning bg-warning m-3" id="Name_xekhach" style="font-weight: 600;">Trách nhiệm</label>
+                        <div>
+                            <label for="" class="fs-5 px-3 pb-1  rounded border border-warning bg-warning" id="Name_xekhach" style="font-weight: 600;">Sạch sẽ</label>
+                            <label for="" class="fs-5 px-3  pb-1  rounded border border-warning bg-warning ms-3" id="Name_xekhach" style="font-weight: 600;">An toàn</label>
                         </div>
+
+
+                        <div class="mt-2">
+                            <label for="" class="fs-5 px-3  pb-1 rounded border border-warning bg-warning" id="Name_xekhach" style="font-weight: 600;">Chất lượng</label>
+                            <label for="" class="fs-5 px-3 pb-1  rounded border border-warning bg-warning ms-3" id="Name_xekhach" style="font-weight: 600;">Trách nhiệm</label>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </fieldset>
-    </form>
+    </div>
 
     <!-- Form đặt vé -->
-    <form class="mt-5">
-        <fieldset class="border border-dark rounded-3 p-3">
-            <legend class="float-none w-auto px-3">Thông tin người đặt:</legend>
-
-            <div class="row d-flex justify-content-between">
-                <div class="row mb-5">
-                    <div class="col-md-5 ms-5">
-                        <label class="col-sm-4 ms-2 me-5 col-form-label">Họ tên người đặt:</label>
-                        <label class="" for="" id="">Tổng giá vé: </label>
+    <div>
+        <form class="mt-5" action="./datve/khachhangdatve" method="POST">
+            <fieldset class="border border-dark rounded-3 p-3">
+                <legend class="float-none w-auto fw-bold px-3">Thông tin người đặt vé</legend>
+                <div class="row d-flex justify-content-between">
+                    <div class="row mb-5">
+                        <div class="ms-5">
+                            <label class="ms-2" for="" id="">Số lượng vé: </label> <input type="number">
+                            <label class="ms-5" for="">Tổng giá vé: </label><label>1000000 Đồng</label>
+                        </div>
+                    </div>
+                    <div class="col-5 ms-5">
+                        <div class="mb-3 row">
+                            <label class="col-sm-4 ms-2 col-form-label">Họ tên người đặt:</label>
+                            <div class="col-sm-7">
+                                <input type="text" id="username" name="username" class="form-control border-dark" placeholder="Enter your name">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label class="col-sm-4 ms-2 col-form-label">Số điện thoại:</label>
+                            <div class="col-sm-7">
+                                <input type="text" id="phone" name="phone" class="form-control border-dark" placeholder="Enter phonenumber">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label class="col-sm-4 ms-2 col-form-label">CCCD:</label>
+                            <div class="col-sm-7">
+                                <input type="text" id="cccd" name="cccd" class="form-control border-dark" placeholder="Enter CMT/CCCD">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <div class="mb-3 row">
+                            <label class="col-sm-3 col-form-label">Điểm đón:</label>
+                            <div class="col-sm-7">
+                                <input id="address" name="address" type="text" class="form-control border-dark" placeholder="Enter address">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label class="col-sm-2 me-4 col-form-label">Email:</label>
+                            <div class="col-sm-7">
+                                <input type="text" id="email" name="email" class="form-control border-dark ms-3" placeholder="Enter email">
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-5 ms-5">
-                    <div class="mb-3 row">
-                        <label class="col-sm-4 ms-2 col-form-label">Họ tên người đặt:</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control border-dark" id="username">
-                        </div>
+                <div class="d-flex justify-content-center pt-5">
+                    <div class="col-2 text-center">
+                        <button type="submit" name="btnhuy" class="btn btn-danger px-4">Huỷ</button>
                     </div>
-                    <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-4 ms-2 col-form-label">Số điện thoại:</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control border-dark" id="phone">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-4 ms-2 col-form-label">CCCD:</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control border-dark" id="cccd">
-                        </div>
+                    <div class="col-2 text-center">
+                        <button type="submit" name="btnthanhtoan" class="btn btn-primary">Thanh toán</button>
                     </div>
                 </div>
-                <div class="col-5">
-                    <div class="mb-3 row">
-                        <label class="col-sm-3 col-form-label">Điểm đón:</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control border-dark" id="address">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 me-4 col-form-label">Email:</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control border-dark ms-3" id="email">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="d-flex justify-content-center pt-5">
-                <div class="col-2 text-center">
-                    <button type="button" class="btn btn-danger px-4">Huỷ</button>
-                </div>
-                <div class="col-2 text-center">
-                    <button type="button" class="btn btn-primary" id="submit">Thanh toán</button>
-                </div>
-            </div>
-        </fieldset>
-    </form>
+
+            </fieldset>
+        </form>
+    </div>
+
 </div>
-<script>
-    $(document).ready(function() {
-        $('#submit').click(function() {
-            $username = $('#username').val();
-            $phone = $('#phone').val();
-            $cccd = $('#cccd').val();
-            $address = $('#address').val();
-            $email = $('#email').val();
-            if ($username == "" || $cccd == "" || $phone == "" || $email == "" || $address == "") {
-                alert("Vui lòng nhập đủ thông tin");
-                return;
-            } else {
-                return $.ajax({
-                    type: "post",
-                    url: "wibu/core/email/process/process_datvexe.php",
-                    data: {
-                        username: $username,
-                        phone: $phone,
-                        cccd: $cccd,
-                        address: $address,
-                        email: $email,
-
-                    },
-                    success: function(response) {
-                        alert(response)
-                    }
-                });
-            }
-
-        });
-    });
-</script>
