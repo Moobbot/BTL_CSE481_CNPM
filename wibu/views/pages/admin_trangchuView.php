f5f830c3362017eb462d872588f6ee65a5c747e3
loofeht <phu832001@gmail.com>
2022-03-13 16:36:34 +0700

    cập nhật trang chủ

--- wibu/views/pages/admin_trangchuView.php
+++ wibu/views/pages/admin_trangchuView.php
@@ -1,14 +1,14 @@
-<div class="col col-lg-12">
-    <div class="row mx-0 mt-3 justify-content-around">
+<div class="col col-lg-12 pt-5">
+    <div class="row mx-0 mt-5 justify-content-evenly">
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
-            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-user-graduate"></i></div>
+            <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
             <div class="ps-3 my-3">
                 <p>Nhà xe liên kết</p>
                 <p> 0 </p>
             </div>
         </div>
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
-            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-user-tie"></i></div>
+            <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
             <div class="ps-3 my-3">
                 <p>Số xe hiện có</p>
                 <p> 0 </p>
@@ -16,16 +16,16 @@
         </div>
     </div>
 
-    <div class="row mx-0 mt-3 justify-content-around">
+    <div class="row mx-0 mt-5 justify-content-evenly">
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
-            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-book-open"></i></div>
+            <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
             <div class="ps-3 my-3">
                 <p>Tổng doanh thu</p>
                 <p> 0 </p>
             </div>
         </div>
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
-            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-school"></i></div>
+            <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
             <div class="ps-3 my-3">
                 <p>Tổng số vé</p>
                 <p> 0 </p>
