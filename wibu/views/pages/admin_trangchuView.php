f856a5ee9420c230d6b59189baa7123c78925e1e
loofeht <phu832001@gmail.com>
2022-03-13 16:41:41 +0700

    in đậm trang chủ

--- wibu/views/pages/admin_trangchuView.php
+++ wibu/views/pages/admin_trangchuView.php
@@ -2,16 +2,16 @@
     <div class="row mx-0 mt-5 justify-content-evenly">
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
             <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
-            <div class="ps-3 my-3">
-                <p>Nhà xe liên kết</p>
-                <p> 0 </p>
+            <div class="my-4">
+                <p class="fw-bolder">Nhà xe liên kết</p>
+                <p class="fw-bolder"> 0 </p>
             </div>
         </div>
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
             <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
-            <div class="ps-3 my-3">
-                <p>Số xe hiện có</p>
-                <p> 0 </p>
+            <div class="ps-3 my-4">
+                <p class="fw-bolder">Số xe hiện có</p>
+                <p class="fw-bolder"> 0 </p>
             </div>
         </div>
     </div>
@@ -19,16 +19,16 @@
     <div class="row mx-0 mt-5 justify-content-evenly">
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
             <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
-            <div class="ps-3 my-3">
-                <p>Tổng doanh thu</p>
-                <p> 0 </p>
+            <div class="ps-3 my-4">
+                <p class="fw-bolder">Tổng doanh thu</p>
+                <p class="fw-bolder"> 0 </p>
             </div>
         </div>
         <div class="col-md-2 col-12 bg-info d-flex mb-2 rounded justify-content-center">
             <div class="mt-3 mb-3" style="font-size: 1.875rem"></div>
-            <div class="ps-3 my-3">
-                <p>Tổng số vé</p>
-                <p> 0 </p>
+            <div class="ps-3 my-4">
+                <p class="fw-bolder">Tổng số vé</p>
+                <p class="fw-bolder"> 0 </p>
             </div>
         </div>
     </div>
